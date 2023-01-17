89efb3b60ee8a706280d728e190310298b831aa4
Emmanuel DK  Dolo <emmanueldkdolo1993@gmail.com>
2023-01-17 04:46:26 +0000

    completed pre 3 to 5

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.Write("What is the magic number? ");  
+        int.Parse(Console.ReadLine());  
+
+        
+        Console.Write("What is your guess?"); 
+        int.Parse(Console.ReadLine());
+    } 
+
 }
